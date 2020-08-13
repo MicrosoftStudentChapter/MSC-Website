58079fe2bc3542e3213fedb7c88af58cc1f85dde
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2020-08-14 00:40:09 +0530

    app ready for deployment

--- website/settings/common.py
+++ website/settings/common.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -107,4 +109,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
+
+# Simplified static file serving.
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
--- website/settings/prod.py
+++ website/settings/prod.py
@@ -1,8 +1,14 @@
 from website.settings.common import *  # noqa
 import os
+import dj_database_url
+
 
 DEBUG = False
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 ALLOWED_HOSTS = []
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+
+DATABASES["default"].update(db_from_env)  # noqa
