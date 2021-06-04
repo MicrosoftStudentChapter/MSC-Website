a7ee623e0d0b166cbe7e9dcfa26a9bc73ba06bbf
Kamalpreet-3223 <ksingh7_be20@thapar.edu>
2021-06-05 03:33:22 +0530

    Member Class and Secretary's Objects created

--- members/views.py
+++ members/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .teamMembers import Secretaries, JointSecretaries
 
 
 def alumni(request):
@@ -6,4 +7,20 @@ def alumni(request):
 
 
 def team(request):
-    return render(request, "members/about.html")
+    # secretaries = {"Pranjal": Pranjal,
+    #                "Arpit": Arpit}
+
+    #details = secretaries.values()
+    sec = Secretaries()
+    jsec = JointSecretaries()
+
+    return render(request, "members/about.html", context={'secretaries': sec, 'jointsec': jsec})
+
+# ONLY FOR TESTING PURPOSE
+
+# secretaries = {"Pranjal": {"name": "Pranjal", "post": "gen sec"},
+#                "Arpit": {"name": "Arpit", "post": "Fin sec"}}
+
+# for detail in secretaries.values():
+#     print(detail["name"])
+#     print(detail["post"])
